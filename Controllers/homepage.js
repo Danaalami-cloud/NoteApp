367ed53529a5caeb1537434274fdc67b6de96973
Freddie Brewin <freddiebrewin@live.com>
2021-06-06 14:42:53 +0100

    printing data to page

--- Controllers/homepage.js
+++ Controllers/homepage.js
@@ -1,22 +1,70 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Entry } = require('../models');
+const withAuth = require('../utils/auth')
 
 router.get('/', async (req, res) => {
     res.render('login');
-})
+});
 
 router.get('/login', async (req, res) => {
     res.render('login');
-})
+});
 
-router.get('/dashboard', async (req, res) => {
+/* router.get('/dashboard', async (req, res) => {
     const userData = await User.findOne({
         where: {
             id: req.session.user_id
         }
     })
-    console.log(userData);
-    res.render('dashboard');
-})
+    const entryData = await Entry.findOne({
+        where: {
+            id: req.session.user_id
+        }
+    });
+    const entrys = entryData.map((entry) => entry.get({ plain:true}))
+    console.log(userData); 
+    console.log(entryData);
+    res.render('dashboard', {
+        entrys,
+        logged_in: req.session.logged_in
+    });
+}) */
+
+// A route to render the dashboard page, only for a logged in user
+router.get('/dashboard', withAuth, (req, res) => {
+    // All of the users posts are obtained from the database
+    Entry.findAll({
+      where: {
+        // use the ID from the session
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'current_date',
+        'water',
+        'exercise',
+        'sleep',
+        'mood',
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['name']
+        },
+      ]
+    })
+      .then(entryData => {
+        // serialize data before passing to template
+        console.log(entryData[0].water);
+        const entrys = entryData.map(entry => entry.get({ plain: true }));
+        console.log(entrys); 
+        /* console.log(entryData); */
+        res.render('dashboard', { entrys, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,13 @@
 const User = require('./user');
-
 const Day = require('./Day');
+const Entry = require('./Entry');
 
+User.hasMany(Entry, {
+    foreignKey: 'user_id',
+});
 
-module.exports = { User, Day };
+Entry.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Day, Entry };
