a7c3af484d1ecf37013d7930fc5f60ed9ba6a397
Freddie Brewin <freddiebrewin@live.com>
2021-06-08 15:41:36 +0100

    Adding leap year functionality

--- public/javscript/dashboard.js
+++ public/javscript/dashboard.js
@@ -115,7 +115,8 @@ const updateNotes = async (event) => {
   }
 }
 
-//Date picker
+//Date picker script
+//Select all required elements from the html
 const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_element = document.querySelector('.date-picker .dates');
@@ -124,29 +125,33 @@ const next_mth_element = document.querySelector('.date-picker .dates .month .nex
 const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
 const days_element = document.querySelector('.date-picker .dates .days');
 
+
+//Create months array
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-let date = new Date();
-let day = date.getDate();
-let month = date.getMonth();
-let year = date.getFullYear();
+let date = new Date(); //Create new date object
+let day = date.getDate(); //Get day from date object
+let month = date.getMonth(); //Get month from date object
+let year = date.getFullYear(); //Get year from date object
 
+//create variables for selected date, day, month, years
 let selectedDate = date;
 let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-mth_element.textContent = months[month] + ' ' + year;
 
-selected_date_element.textContent = formatDate(date);
-selected_date_element.dataset.value = selectedDate;
+mth_element.textContent = months[month] + ' ' + year; //Set month element text content
 
-populateDates();
+selected_date_element.textContent = formatDate(date); //Set selected date element text content
+selected_date_element.dataset.value = selectedDate; // Get value of the selected_date_element and set it equal to selectedDate
+
+populateDates(); 
 
 // EVENT LISTENERS
 /* date_picker_element.addEventListener('click', toggleDatePicker); */
-next_mth_element.addEventListener('click', goToNextMonth);
-prev_mth_element.addEventListener('click', goToPrevMonth);
+next_mth_element.addEventListener('click', goToNextMonth); //Add event listeners to go to next month
+prev_mth_element.addEventListener('click', goToPrevMonth); //Add event listeners to go to previous month
 
 // FUNCTIONS
 /* function toggleDatePicker (e) {
@@ -155,32 +160,82 @@ prev_mth_element.addEventListener('click', goToPrevMonth);
 	}
 } */
 
+//Next month function
 function goToNextMonth (e) {
-	month++;
-	if (month > 11) {
+	month++; //Increment the month value
+	if (month > 11) { //If this value exceeds 11, set month back to 0 and increment the year
 		month = 0;
 		year++;
 	}
-	mth_element.textContent = months[month] + ' ' + year;
-	populateDates();
+	mth_element.textContent = months[month] + ' ' + year; //populate new month element 
+	populateDates(); //populate new dates
 }
 
+
+//Previous month function
 function goToPrevMonth (e) {
-	month--;
-	if (month < 0) {
+	month--; //Reduce month value by 1 
+	if (month < 0) { //If value goes below 0, set value to 11 and take 1 off the year
 		month = 11;
 		year--;
 	}
-	mth_element.textContent = months[month] + ' ' + year;
-	populateDates();
+	mth_element.textContent = months[month] + ' ' + year; //populate new month element
+	populateDates(); //populate new dates
 }
 
-function populateDates (e) {
-	days_element.innerHTML = '';
-	let amount_days = 31;
+//Calculating leap year function
+function leapYear(selectedYear)
+{
+  return ((selectedYear % 4 == 0) && (selectedYear % 100 != 0)) || (selectedYear % 400 == 0);
+}
 
+
+
+//Populate dates function
+function populateDates (e) {
+	days_element.innerHTML = ''; //Set days_element to be empty
+	let amount_days = 31; //create amount of days variable
+  //Defining days in each month
+  if(month == 0) {
+    amount_days = 30;
+  }
 	if (month == 1) {
-		amount_days = 28;
+    isLeapYear = leapYear(year);
+    if (isLeapYear === true) {
+      amount_days = 29;
+    } else {
+      amount_days = 28;
+    }
+	}
+  if (month == 2) {
+		amount_days = 31;
+	}
+  if (month == 3) {
+		amount_days = 30;
+	}
+  if (month == 4) {
+		amount_days = 31;
+	}
+  if (month == 5) {
+		amount_days = 30;
+	}
+  if (month == 6) {
+		amount_days = 31;
+	}
+  if (month == 7) {
+		amount_days = 31;
+	}
+  if (month == 8) {
+		amount_days = 30;
+	}
+  if (month == 9) {
+		amount_days = 31;
+	}
+  if (month == 10) {
+		amount_days = 30;
+	}
+  if (month == 11) {
+		amount_days = 31;
 	}
 
 	for (let i = 0; i < amount_days; i++) {
